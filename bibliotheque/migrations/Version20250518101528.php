e57a7412d407921b683839395eebe11cb00a9cd4
ahmataaffadine99 <ahmataffadine82@gmail.com>
2025-05-18 14:25:36 +0200

    Suppression des commentaires dans le fichier de migration

--- bibliotheque/migrations/Version20250518101528.php
+++ bibliotheque/migrations/Version20250518101528.php
@@ -7,9 +7,7 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
+
 final class Version20250518101528 extends AbstractMigration
 {
     public function getDescription(): string
@@ -19,7 +17,6 @@ final class Version20250518101528 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql(<<<'SQL'
             CREATE TABLE auteur (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, prenom VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
         SQL);
@@ -63,7 +60,6 @@ final class Version20250518101528 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql(<<<'SQL'
             ALTER TABLE discussion DROP FOREIGN KEY FK_C0B9F90F60BB6FE6
         SQL);
